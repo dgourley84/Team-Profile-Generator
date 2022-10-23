e97d861e6f3f7fa8584ba3d8a705ae31e105361e
dgourley84 <81537606+dgourley84@users.noreply.github.com>
2022-10-23 14:48:12 +1000

    Addition of testing protocols

---
+++ test/Manager.test.js
@@ -0,0 +1,20 @@
+const Manager = require("../lib/manager");
+const Employee = require("../lib/employee");
+
+test("Can set office number via constructor argument", () => {
+  const testValue = 100;
+  const manager = new Manager("Foo", 1, "test@test.com", testValue);
+  expect(manager.officeNumber).toBe(testValue);
+});
+
+test('getRole() should return "Manager"', () => {
+  const testValue = "Manager";
+  const manager = new Manager("Foo", 1, "test@test.com", 100);
+  expect(manager.getRole()).toBe(testValue);
+});
+
+test("Can get office number via getOffice()", () => {
+  const testValue = 100;
+  const manager = new Manager("Foo", 1, "test@test.com", testValue);
+  expect(manager.getOfficeNumber()).toBe(testValue);
+});
